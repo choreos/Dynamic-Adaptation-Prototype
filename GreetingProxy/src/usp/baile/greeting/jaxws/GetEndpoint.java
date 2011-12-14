c01969a9a069e05caa812516289085c7617d6622
Leonardo Leite <leonardofl87@gmail.com>
2011-12-14 10:53:02 -0200

    wsgen run

---
+++ GreetingProxy/src/usp/baile/greeting/jaxws/GetEndpoint.java
@@ -0,0 +1,15 @@
+
+package usp.baile.greeting.jaxws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "getEndpoint", namespace = "http://greeting.baile.usp/")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getEndpoint", namespace = "http://greeting.baile.usp/")
+public class GetEndpoint {
+
+
+}
---
+++ GreetingProxy/src/usp/baile/greeting/jaxws/Greet.java
@@ -0,0 +1,36 @@
+
+package usp.baile.greeting.jaxws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "greet", namespace = "http://greeting.baile.usp/")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "greet", namespace = "http://greeting.baile.usp/")
+public class Greet {
+
+    @XmlElement(name = "arg0", namespace = "")
+    private String arg0;
+
+    /**
+     * 
+     * @return
+     *     returns String
+     */
+    public String getArg0() {
+        return this.arg0;
+    }
+
+    /**
+     * 
+     * @param arg0
+     *     the value for the arg0 property
+     */
+    public void setArg0(String arg0) {
+        this.arg0 = arg0;
+    }
+
+}
