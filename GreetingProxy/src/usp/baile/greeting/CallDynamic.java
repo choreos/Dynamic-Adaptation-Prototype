701fb5e1dafff8c8c2d3ea3dc75853874d83dc26
Leonardo Leite <leonardofl87@gmail.com>
2011-12-14 16:37:39 -0200

    CallDynamic now calls the BPEL composition

--- GreetingProxy/src/usp/baile/greeting/CallDynamic.java
+++ GreetingProxy/src/usp/baile/greeting/CallDynamic.java
@@ -10,16 +10,21 @@ import eu.choreos.vv.exceptions.FrameworkException;
 import eu.choreos.vv.exceptions.InvalidOperationNameException;
 import eu.choreos.vv.exceptions.WSDLException;
 
+/**
+ *  Invoke the BPEL composition that uses the proxy service
+ *
+ */
 public class CallDynamic {
 
 	public static void main(String[] args) throws WSDLException, XmlException, IOException, FrameworkException, InvalidOperationNameException, NoSuchFieldException {
 
-		WSClient dynamicClient = new WSClient("http://localhost:1237/greeting?wsdl");
+		// the BPEL composition was deployed on Petals
+		WSClient dynamicClient = new WSClient("http://localhost:8084/petals/services/DynamicGreeting?wsdl");
 		
 		String[] responses = new String[3];
 		for (int i=0; i<3; i++) {
-			Item item = dynamicClient.request("greet", "Dejan");
-			responses[i] = item.getChild("return").getContent();
+			Item item = dynamicClient.request("process", "Dejan");
+			responses[i] = item.getChild("result").getContent();
 		}
 		for (String response: responses)
 			System.out.println(response);
